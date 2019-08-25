d1750b2495170edceb98928e88ed36da7032e725
Dave Vane <davevane@gmail.com>
2019-08-25 12:11:38 +1000

    Source files uploaded
    A "Personal Information Exchange" file was omitted from the upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,380 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(26, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Age"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(27, 387)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 37)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "E&xit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(629, 366)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(144, 58)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "&Start Test"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(38, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(71, 35)
+        Me.TextBox1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(27, 193)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Pen Drop Results"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(60, 66)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(58, 17)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "Drop 3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(60, 41)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(58, 17)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Drop 2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(60, 18)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(58, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Drop 1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(585, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Speed (seconds)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(602, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(602, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(602, 120)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "0"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(27, 85)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 83)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Sex"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(55, 41)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(59, 17)
+        Me.RadioButton2.TabIndex = 15
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Female"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(55, 18)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(48, 17)
+        Me.RadioButton1.TabIndex = 14
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Male"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(350, 86)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(175, 55)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Label6"
+        Me.Label6.Visible = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(365, 61)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(144, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Your Average Reaction Time"
+        Me.Label7.Visible = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(311, 227)
+        Me.ProgressBar1.Maximum = 0
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(377, 23)
+        Me.ProgressBar1.Step = 0
+        Me.ProgressBar1.TabIndex = 14
+        Me.ProgressBar1.Visible = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(260, 233)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(47, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Average"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label8.Visible = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(458, 211)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(36, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Faster"
+        Me.Label9.Visible = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(602, 211)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(39, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Slower"
+        Me.Label10.Visible = False
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(311, 256)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(377, 23)
+        Me.ProgressBar2.TabIndex = 19
+        Me.ProgressBar2.Visible = False
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(278, 260)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(26, 13)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "You"
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label11.Visible = False
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(308, 212)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(13, 13)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "0"
+        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label12.Visible = False
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(308, 292)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(36, 13)
+        Me.Label13.TabIndex = 22
+        Me.Label13.Text = "Faster"
+        Me.Label13.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label13.Visible = False
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(650, 292)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(39, 13)
+        Me.Label14.TabIndex = 23
+        Me.Label14.Text = "Slower"
+        Me.Label14.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label14.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Science!"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,217 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Sex As String
+        Dim press1 As Double = 0
+        Dim press2 As Double = 0
+        Dim press3 As Double = 0
+        Dim pressStart As Double = 0
+        'Dim press2Start As Double = 0
+        'Dim press3Start As Double = 0
+        If RadioButton1.Checked Then Sex = "Male"
+        If RadioButton2.Checked Then Sex = "Female"
+
+
+
+        Dim rng As New Random
+        Dim arndint = rng.Next(2, 6)
+
+        If TimeOfDay >= #11:59:55 PM# Then
+            MsgBox("The current time is within 5 seconds of midnight" &
+                   vbCrLf & "The timer returns to 0.0 at midnight")
+            Return
+        End If
+        Dim start, finish, totalTime As Double
+        Dim Msgboxstring As String
+
+        Msgboxstring = "Ready to begin?"
+        If (MsgBox(Msgboxstring, MsgBoxStyle.YesNo, "Science Project")) = MsgBoxResult.Yes Then
+            'Dim rng As New Random
+            arndint = rng.Next(2, 6)
+            start = Microsoft.VisualBasic.DateAndTime.Timer                     'current time
+            finish = start + arndint                                            'Set end time for random duration.
+            Do While Microsoft.VisualBasic.DateAndTime.Timer < finish
+                ' Do other processing while waiting for 5 seconds to elapse.
+            Loop
+            BackColor = Color.DeepSkyBlue                                       'time has passed, so change colour and start first timer
+            pressStart = Microsoft.VisualBasic.DateAndTime.Timer
+            'wait until the key is pressed
+            MsgBox("presskey", MsgBoxStyle.Critical, "Science!")
+            press1 = Microsoft.VisualBasic.DateAndTime.Timer - pressStart
+            Me.Label3.Text = Math.Round(press1, 4)                                          'update screen time
+            BackColor = Color.WhiteSmoke                                        'reset screen colour
+
+        End If
+
+        Msgboxstring = "Ready for next test?"
+        If (MsgBox(Msgboxstring, MsgBoxStyle.YesNo, "Science!")) = MsgBoxResult.Yes Then
+            'Dim rng As New Random
+            arndint = rng.Next(2, 6)
+            start = Microsoft.VisualBasic.DateAndTime.Timer                     'current time
+            finish = start + arndint                                            'Set end time for random duration.
+            Do While Microsoft.VisualBasic.DateAndTime.Timer < finish
+                ' Do other processing while waiting for 5 seconds to elapse.
+            Loop
+            BackColor = Color.DeepSkyBlue                                       'time has passed, so change colour and start first timer
+            pressStart = Microsoft.VisualBasic.DateAndTime.Timer
+            'wait until the key is pressed
+            MsgBox("presskey", MsgBoxStyle.Critical, "Science!")
+            press2 = Microsoft.VisualBasic.DateAndTime.Timer - pressStart
+            Me.Label4.Text = Math.Round(press2, 4)                                      'update screen time
+            BackColor = Color.WhiteSmoke                                        'reset screen colour
+
+        End If
+
+
+
+        Msgboxstring = "Ready for final test?"
+        If (MsgBox(Msgboxstring, MsgBoxStyle.YesNo, "Science!")) = MsgBoxResult.Yes Then
+            'Dim rng As New Random
+            arndint = rng.Next(2, 6)
+            start = Microsoft.VisualBasic.DateAndTime.Timer                     'current time
+            finish = start + arndint                                            'Set end time for random duration.
+            Do While Microsoft.VisualBasic.DateAndTime.Timer < finish
+                ' Do other processing while waiting for 5 seconds to elapse.
+            Loop
+            BackColor = Color.DeepSkyBlue                                       'time has passed, so change colour and start first timer
+            pressStart = Microsoft.VisualBasic.DateAndTime.Timer
+            'wait until the key is pressed
+            MsgBox("presskey", MsgBoxStyle.Critical, "Science!")
+            press3 = Microsoft.VisualBasic.DateAndTime.Timer - pressStart
+            Me.Label5.Text = Math.Round(press3, 4)                                             'update screen time
+            BackColor = Color.WhiteSmoke                                        'reset screen colour
+
+        End If
+
+        'average times of key press
+        Dim pressAverage As Double = (press1 + press2 + press3) / 3
+        Me.Label6.Text = Math.Round(pressAverage, 4)
+        Me.Label6.Visible = True
+        Me.Label7.Visible = True
+        Me.Label8.Visible = True
+        Me.Label9.Visible = True
+        Me.Label10.Visible = True
+        Me.Label11.Visible = True
+        Me.ProgressBar1.Visible = True
+        Me.ProgressBar2.Visible = True
+
+        Dim myPath As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Science.csv"
+        My.Computer.FileSystem.WriteAllText(myPath,
+                                            vbCrLf & Me.TextBox1.Text & "," & Sex & "," & Me.CheckBox1.Checked & "," & Me.CheckBox2.Checked & "," & Me.CheckBox3.Checked & "," & press1 & "," & press2 & "," & press3 & "," & pressAverage, True)
+
+
+
+        'put the code for average progress bars here
+        'Dim myPath As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Science.csv"
+        Using MyReader As New Microsoft.VisualBasic.
+            FileIO.TextFieldParser(
+                myPath)
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(",")
+            Dim currentRow As String()
+            Dim totalRows As Short = 0
+            Dim largestNumber As Double = 0
+            Dim runningTotal As Double = 0
+            Dim ageOnRecord As Double = 0
+            While Not MyReader.EndOfData
+                Try
+
+                    currentRow = MyReader.ReadFields()
+                    Dim currentField As String
+                    Dim fieldCount As Short = 1
+                    For Each currentField In currentRow
+                        'processing of each of the fields goes here
+                        If (fieldCount = 1) Then
+                            ageOnRecord = Convert.ToInt32(currentField)
+                            'MsgBox(ageOnRecord,, "ageOnRecord")
+                        End If
+                        If (fieldCount = 9) Then
+                            If (ageOnRecord >= (Convert.ToInt32(Me.TextBox1.Text) - 3) And ageOnRecord <= (Convert.ToInt32(Me.TextBox1.Text) + 3)) Then
+                                'MsgBox(Convert.ToInt32(Me.TextBox1.Text),, "textbox age")
+                                'MsgBox(currentField,, "speed")
+                                totalRows = totalRows + 1
+                                runningTotal = runningTotal + currentField
+                                If (currentField > largestNumber) Then
+                                    largestNumber = currentField
+                                End If
+                            End If
+                        End If
+                        fieldCount = fieldCount + 1
+                    Next
+
+                Catch ex As Microsoft.VisualBasic.
+                    FileIO.MalformedLineException
+                    MsgBox("Line " & ex.Message &
+                           "is not valid and will be skipped.")
+                End Try
+
+            End While
+            'MsgBox(totalRows,, "Total Rows")
+            'MsgBox(largestNumber,, "Largest Number")
+            'MsgBox(runningTotal,, "Total")
+            'MsgBox(runningTotal / totalRows,, "Average Value")
+
+
+
+
+            Me.Label8.Visible = True
+            Me.Label9.Visible = True
+            Me.Label9.Text = "Average: " & Math.Round(runningTotal / totalRows, 4)
+            Me.Label10.Visible = True
+            Me.Label10.Text = "Max: " & Math.Round(largestNumber, 4)
+            Me.Label11.Visible = True
+            Me.Label13.Visible = True
+            Me.Label14.Visible = True
+            Me.ProgressBar1.Visible = True
+            Me.ProgressBar2.Visible = True
+            Me.ProgressBar1.Minimum = 0
+            Me.ProgressBar1.Maximum = largestNumber * 1000
+            Me.ProgressBar1.Value = (runningTotal * 1000) / totalRows
+            Me.ProgressBar2.Minimum = 0
+            Me.ProgressBar2.Maximum = largestNumber * 1000
+            Me.ProgressBar2.Value = pressAverage * 1000
+
+        End Using
+
+        MsgBox("Reset Form?", MsgBoxStyle.OkOnly, "Science!")
+
+
+        BackColor = Color.WhiteSmoke
+        Me.Label3.Text = 0
+        Me.Label4.Text = 0
+        Me.Label5.Text = 0
+        Me.Label9.Text = 0
+        Me.Label10.Text = 0
+        Me.TextBox1.Text = ""
+        Me.CheckBox3.CheckState = False
+        Me.CheckBox2.CheckState = False
+        Me.CheckBox1.CheckState = False
+        Me.RadioButton1.Checked = False
+        Me.RadioButton2.Checked = False
+        Me.Label6.Visible = False
+        Me.Label7.Visible = False
+        Me.Label8.Visible = False
+        Me.Label9.Visible = False
+        Me.Label10.Visible = False
+        Me.Label11.Visible = False
+        Me.Label13.Visible = False
+        Me.Label14.Visible = False
+        Me.ProgressBar1.Visible = False
+        Me.ProgressBar2.Visible = False
+        Me.ProgressBar1.Minimum = 0
+        Me.ProgressBar1.Maximum = 0
+        Me.ProgressBar1.Value = 0
+        Me.ProgressBar2.Minimum = 0
+        Me.ProgressBar2.Maximum = 0
+        Me.ProgressBar2.Value = 0
+
+
+
+
+    End Sub
+
+End Class
